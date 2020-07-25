db14d5c611581a40fc18892081665f1aa8740e86
Tri Wardana <mtriwardanaa@gmail.com>
2020-07-25 14:31:50 +0700

    https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -15,8 +15,10 @@ class HttpsProtocol
      */
     public function handle($request, Closure $next)
     {
-    	if (!$request->secure()) {
-            return redirect()->secure($request->getRequestUri());
+        if (env('APP_ENV') != 'local') {
+            if (!$request->secure()) {
+                return redirect()->secure($request->getRequestUri());
+            }
         }
             
         return $next($request);
