85766353ae3703f2bcc6025e81fb5ccb0ee6f012
Tri Wardana <mtriwardanaa@gmail.com>
2020-07-28 15:22:24 +0700

    dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -11,8 +11,6 @@ use App\BarangMasuk;
 use App\BarangKeluar;
 use App\Barang;
 
-use App\Order;
-
 class Dashboard extends Controller
 {
 	public function __construct()
@@ -22,7 +20,23 @@ class Dashboard extends Controller
     
     public function dashboard()
     {
+    	$id_divisi = Auth::user()->id_divisi;
+    	$id_kategori = Auth::user()->id_kategori;
 
+    	if ($id_divisi == 10) {
+            $order_pending      = Order::whereNull('approved_by')->count();
+            $total_barang_masuk = BarangMasuk::count();
+            $total_barang_keluar= BarangKeluar::count();
+            $barang             = Barang::where('qty_barang', '<', DB::raw('warning_stok'))->count();
+
+            $data = [
+                'order_pending'      => $order_pending,
+                'total_barang_masuk' => $total_barang_masuk,
+                'total_barang_keluar'=> $total_barang_keluar,
+                'barang'             => $barang,
+            ];
+
+	    	return view('dashboard_ga', $data);
     	}
 
     	$total_order = Order::where('id_divisi', $id_divisi)->where('id_kategori', $id_kategori)->count();
